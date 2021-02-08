1c2100c5b95f9e5bd30ed033baf78c2390407414
devMuratCengiz <78637127+devMuratCengiz@users.noreply.github.com>
2021-02-08 21:06:42 +0300

    Add files via upload

---
+++ ClassMetotDemo/ClassMetotDemo/CustomerManager.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class CustomerManager
+    {
+        public void Add(Customer customer)  
+        {
+            Console.WriteLine(customer.CustomerId+" Id numarali kullanici "+
+                customer.FirstName+" "+customer.LastName + " sisteme eklenmistir.");
+        }
+        public void Delete(Customer customer)
+        {
+            Console.WriteLine(customer.CustomerId + " Id numaralı kullanici " + customer.FirstName + " " + customer.LastName + " sistemden silinmistir.");
+        }
+    }
+}
---
+++ ClassMetotDemo/ClassMetotDemo/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer1 = new Customer();
+            customer1.CustomerId = 1;
+            customer1.FirstName = "Murat";
+            customer1.LastName = "Cengiz";
+            customer1.CustomerJob = "Student";
+
+            Customer[] customers = new Customer[] {customer1};
+
+            foreach (var customer in customers)
+            {
+                Console.WriteLine("Id : "+customer.CustomerId+"\nFirst Name : "+customer.FirstName+"\nLast Name : "+customer.LastName+"\nCustomer Job : "+customer.CustomerJob);
+            }
+            CustomerManager customerManager = new CustomerManager();
+            customerManager.Add(customer1);
+
+            CustomerManager customerManager1 = new CustomerManager();
+            customerManager.Delete(customer1);
+        }
+    }
+}
